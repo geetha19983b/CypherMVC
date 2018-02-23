d824c527b2aac8e70bc88b159c7d14214850b75b
Geethagajalakshmi_B <Geethagajalakshmi_B@CHNMCT142291L>
2018-02-23 15:02:31 +0530

    Add project files.

---
+++ CypherMessageDashboard/Controllers/MessagesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CypherMessageDashboard.Controllers
+{
+    public class MessagesController : Controller
+    {
+        // GET: Messages
+        public ActionResult ViewAll()
+        {
+            return View();
+        }
+        public ActionResult Reply()
+        {
+            return View();
+        }
+    }
+}
